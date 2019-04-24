b221e5c94bfcb6f8b33748cc6ec220b46d073cdd
kingdo777 <1440852110@qq.com>
2019-04-24 11:42:43 +0000

    第一次测试，代码创建lambda函数

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ fp.create_function(zipped_code_path, func_handler)
 # 调用函数
 respond = fp.invoke_function()
 with open("logfile", "a+") as f:
-    f.write(str(respond)+"\n")
+    f.write(fp.func_name + str(respond) + "\n")
 
 # 删除函数
-fp.delete_function()
+# fp.delete_function()
